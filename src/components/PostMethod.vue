c76415c2b8583650eb637d102d58aa7f5aba25cd
Sachi <jiaxinnc@gmail.com>
2023-11-23 17:52:02 +0800

    Post method changed to bootstrap

--- src/components/PostMethod.vue
+++ src/components/PostMethod.vue
@@ -1,37 +1,37 @@
 <template>
   <div class="postmethod">
-    <div class="shadow p-3 mb-5 bg-body-tertiary rounded" style="max-width: fit-content; margin: auto;">
+    <div class="shadow p-3 mb-5 bg-body-tertiary rounded" style="max-width: 40%; margin: auto;">
       <div class="form-container">
         <h1>SMS Form</h1>
-        <form @submit.prevent="submitForm" class="form-label">
-          <div class="row mb-3">
-            <label for="colFormLabel" class="col-sm-2 col-form-label">From</label>
-            <div class="col-sm-20">
-              <input type="email" class="form-control" id="colFormLabel" placeholder="From ">
-            </div>
+        <form @submit.prevent="submitForm">
+          <div class="mb-3 row">
+            <label for="from" class="col-sm-2 col-form-label" style="display: block; margin-bottom: 5px;">From:</label>
+            <div class="col sm-10">
+            <input type="text" v-model="from" class="form-control" id="from" required />
+          </div>
           </div>
 
-          <div class="row mb-3">
-            <label for="colFormLabel" class="col-sm-2 col-form-label">To</label>
-            <div class="col-sm-20">
-              <input type="email" class="form-control" id="colFormLabel" placeholder="To ">
-            </div>
+          <div class="mb-3 row">
+            <label for="to" class="col-sm-2 col-form-label" style="display: block; margin-bottom: 5px;">To:</label>
+            <div class="col sm-10">
+            <input type="text" v-model="to" class="form-control" id="to" required />
+          </div>
           </div>
 
-          <div class="row mb-3">
-            <label for="colFormLabel" class="col-sm-2 col-form-label">Text</label>
-            <div class="col-sm-20">
-              <input type="email" class="form-control" id="colFormLabel" placeholder="Enter your text here">
-            </div>
+          <div class="mb-3 row">
+            <label for="text" class="col-sm-2 col-form-label" style="display: block; margin-bottom: 5px;">Text:</label>
+            <div class="col sm-10">
+            <textarea v-model="text" class="form-control" id="text" required></textarea>
+          </div>
           </div>
 
-          <button type="submit" class="form-button">Send SMS</button>
+          <button type="submit" class="btn btn-primary" style="background-color: red;">Send SMS</button>
         </form>
       </div>
     </div>
   </div>
 </template>
-  
+
 <script>
 export default {
   data() {
@@ -62,7 +62,7 @@ export default {
         // Check if the SMS was successfully added
         if (data.isSuccess) {
           alert('SMS successfully added!');
-
+          
           // Clear the form fields
           this.from = '';
           this.to = '';
@@ -81,12 +81,12 @@ export default {
   
 <style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 
 .postmethod {
@@ -95,9 +95,8 @@ export default {
 }
 
 .form-container {
-  max-width: 400px;
-  /* Adjust the maximum width as needed */
-  margin: 0 auto;
+  max-width: 400px; /* Adjust the maximum width as needed */
+  margin: 0 auto; 
 }
 
 .form-label {
